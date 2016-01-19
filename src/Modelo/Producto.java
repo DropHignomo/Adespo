4a85193c248af6183c1f0f843a777d06c6ef7a41
pedrob1ih <pedrob1ih@gmail.com>
2016-01-19 11:12:18 +0100

    se ha implementado la clase Producto

---
+++ src/Modelo/Producto.java
@@ -0,0 +1,240 @@
+package Modelo;
+
+
+public class Producto {
+    private int codigo;
+    private String denominacion;
+    private String presentacion;
+    private String formato;
+    private int unidades;
+    private String composicion;
+    private int tiempoEspera;
+    private String formaFarmacologica;
+    private int nRegistro;
+    private String nLote;
+    private String fechaCompra;
+    private String fechaCaducidad;
+    private double precio;
+    private double pVP;
+    private int iVA;
+    private String indicaciones;
+    private String instruccioes;
+    private String familia;
+    private String almacen;
+    private String grpVentas;
+    private String codFactura;
+    private String suministrador;
+    private String nAlbaran;
+
+    public Producto() {
+        this.codigo = 0;
+        this.denominacion = "";
+        this.presentacion = "";
+        this.formato = "";
+        this.unidades = 0;
+        this.composicion = "";
+        this.tiempoEspera = 0;
+        this.formaFarmacologica = "";
+        this.nRegistro = 0;
+        this.nLote = "";
+        this.fechaCompra = "";
+        this.fechaCaducidad = "";
+        this.precio = 0;
+        this.pVP = 0;
+        this.iVA = 0;
+        this.indicaciones = "";
+        this.instruccioes = "";
+        this.familia = "";
+        this.almacen = "";
+        this.grpVentas = "";
+        this.codFactura = "";
+        this.suministrador = "";
+        this.nAlbaran = "";
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDenominacion() {
+        return denominacion;
+    }
+
+    public String getPresentacion() {
+        return presentacion;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public String getComposicion() {
+        return composicion;
+    }
+
+    public int getTiempoEspera() {
+        return tiempoEspera;
+    }
+
+    public String getFormaFarmacologica() {
+        return formaFarmacologica;
+    }
+
+    public int getnRegistro() {
+        return nRegistro;
+    }
+
+    public String getnLote() {
+        return nLote;
+    }
+
+    public String getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public String getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public double getpVP() {
+        return pVP;
+    }
+
+    public int getiVA() {
+        return iVA;
+    }
+
+    public String getIndicaciones() {
+        return indicaciones;
+    }
+
+    public String getInstruccioes() {
+        return instruccioes;
+    }
+
+    public String getFamilia() {
+        return familia;
+    }
+
+    public String getAlmacen() {
+        return almacen;
+    }
+
+    public String getGrpVentas() {
+        return grpVentas;
+    }
+
+    public String getCodFactura() {
+        return codFactura;
+    }
+
+    public String getSuministrador() {
+        return suministrador;
+    }
+
+    public String getnAlbaran() {
+        return nAlbaran;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
+
+    public void setPresentacion(String presentacion) {
+        this.presentacion = presentacion;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public void setComposicion(String composicion) {
+        this.composicion = composicion;
+    }
+
+    public void setTiempoEspera(int tiempoEspera) {
+        this.tiempoEspera = tiempoEspera;
+    }
+
+    public void setFormaFarmacologica(String formaFarmacologica) {
+        this.formaFarmacologica = formaFarmacologica;
+    }
+
+    public void setnRegistro(int nRegistro) {
+        this.nRegistro = nRegistro;
+    }
+
+    public void setnLote(String nLote) {
+        this.nLote = nLote;
+    }
+
+    public void setFechaCompra(String fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public void setFechaCaducidad(String fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setpVP(double pVP) {
+        this.pVP = pVP;
+    }
+
+    public void setiVA(int iVA) {
+        this.iVA = iVA;
+    }
+
+    public void setIndicaciones(String indicaciones) {
+        this.indicaciones = indicaciones;
+    }
+
+    public void setInstruccioes(String instruccioes) {
+        this.instruccioes = instruccioes;
+    }
+
+    public void setFamilia(String familia) {
+        this.familia = familia;
+    }
+
+    public void setAlmacen(String almacen) {
+        this.almacen = almacen;
+    }
+
+    public void setGrpVentas(String grpVentas) {
+        this.grpVentas = grpVentas;
+    }
+
+    public void setCodFactura(String codFactura) {
+        this.codFactura = codFactura;
+    }
+
+    public void setSuministrador(String suministrador) {
+        this.suministrador = suministrador;
+    }
+
+    public void setnAlbaran(String nAlbaran) {
+        this.nAlbaran = nAlbaran;
+    }
+    
+    
+}

c296439921162339e4a80142060ca1507a89b938
pedrob1ih <pedrob1ih@gmail.com>
2016-03-20 17:58:29 +0100

    implentando las inserts

--- src/Modelo/Albaran.java
+++ src/Modelo/Albaran.java
@@ -1,48 +1,140 @@
 package Modelo;
 
 //del albarán se debe hacer un reporte con la informacion de la clase
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
 public class Albaran {
-    private int nAlbaran;
-    private String fecha;
-    private int idGanadero;
-    private int idVeterinario;
+    private int cod_albaran;
+    private Date fecha;
+    private String direccion;
+    private int num_explotacion;
+    private String nif;
+    private String poblacion;
+    private String provincia;
+    private int num_animales;
+    private int num_colegiado;
+    private String apellidos;
+    private String nombre;
 
-    public Albaran() {
-        this.nAlbaran = 0;
-        this.fecha = "";
-        this.idGanadero = 0;
-        this.idVeterinario = 0;
+    public Albaran(int cod_albaran, Date fecha, String direccion, int num_explotacion, String nif, String poblacion, String provincia, int num_animales, int num_colegiado, String apellidos, String nombre) {
+        this.cod_albaran = cod_albaran;
+        this.fecha = fecha;
+        this.direccion = direccion;
+        this.num_explotacion = num_explotacion;
+        this.nif = nif;
+        this.poblacion = poblacion;
+        this.provincia = provincia;
+        this.num_animales = num_animales;
+        this.num_colegiado = num_colegiado;
+        this.apellidos = apellidos;
+        this.nombre = nombre;
     }
 
-    public int getnAlbaran() {
-        return nAlbaran;
+    public int getCod_albaran() {
+        return cod_albaran;
     }
 
-    public String getFecha() {
+    public Date getFecha() {
         return fecha;
     }
 
-    public int getIdGanadero() {
-        return idGanadero;
+    public String getDireccion() {
+        return direccion;
     }
 
-    public int getIdVeterinario() {
-        return idVeterinario;
+    public int getNum_explotacion() {
+        return num_explotacion;
     }
 
-    public void setnAlbaran(int nAlbaran) {
-        this.nAlbaran = nAlbaran;
+    public String getNif() {
+        return nif;
     }
 
-    public void setFecha(String fecha) {
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public int getNum_animales() {
+        return num_animales;
+    }
+
+    public int getNum_colegiado() {
+        return num_colegiado;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setCod_albaran(int cod_albaran) {
+        this.cod_albaran = cod_albaran;
+    }
+
+    public void setFecha(Date fecha) {
         this.fecha = fecha;
     }
 
-    public void setIdGanadero(int idGanadero) {
-        this.idGanadero = idGanadero;
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
 
-    public void setIdVeterinario(int idVeterinario) {
-        this.idVeterinario = idVeterinario;
-    }    
+    public void setNum_explotacion(int num_explotacion) {
+        this.num_explotacion = num_explotacion;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public void setNum_animales(int num_animales) {
+        this.num_animales = num_animales;
+    }
+
+    public void setNum_colegiado(int num_colegiado) {
+        this.num_colegiado = num_colegiado;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    public boolean alta() throws SQLException{
+        Statement st=ConexionBD.getInstancia().getSt();
+        String sql="insert into ALBARAN values("
+                +this.cod_albaran+","+this.fecha+","
+                +this.direccion+","+this.num_explotacion+","
+                +this.nif+","+this.poblacion+","+this.provincia+","
+                +this.num_animales+","+this.num_colegiado+","+this.apellidos+
+                ","+this.nombre+")";
+        return st.execute(sql);
+    }
+    public boolean baja() throws SQLException{
+        Statement st=ConexionBD.getInstancia().getSt();
+        String sql="delete from albaran where cod_albaran="+this.cod_albaran;
+        return st.execute(sql);
+    }
 }
--- src/adespo/Adespo.java
+++ src/adespo/Adespo.java
@@ -1,6 +1,9 @@
 package adespo;
 
+import Modelo.Albaran;
 import Modelo.ConexionBD;
+import java.util.Date;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,6 +14,9 @@ public class Adespo {
             ConexionBD bd=ConexionBD.getInstancia();
             bd.abrirConexion();
             bd.crearTablas();
+            Albaran a = new Albaran(1, new Date(), "direccion", 1, "444", "poblacion", "provincia", 88, 66, "apellidos", "nombre");
+            if(a.alta())
+                System.out.println("alta correcta");
         } catch (Exception ex) {
             Logger.getLogger(Adespo.class.getName()).log(Level.SEVERE, null, ex);
         }
